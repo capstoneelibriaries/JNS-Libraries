c25d499f9df79ea47e9847b7db961e66ce1c5f11
jamesjmeyer210 <jamesjmeyer210@gmail.com>
2019-03-11 15:20:39 -0500

    Wrote basic genre class

---
+++ src/main/java/models/Genre.java
@@ -0,0 +1,22 @@
+package models;
+
+public class Genre {
+
+    private String genre;
+
+    public Genre(){
+        // default
+    }
+
+    public String getGenre() {
+        return genre;
+    }
+
+    public void setGenre(String genre){
+        this.genre = genre;
+    }
+
+    public String toString(){
+        return this.genre;
+    }
+}
