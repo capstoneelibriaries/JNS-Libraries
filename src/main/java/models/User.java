28d343f5586ca490e5562d0f2972dd055a02c0a8
jamesjmeyer210 <jamesjmeyer210@gmail.com>
2019-03-11 15:07:30 -0500

    Wrote user class

--- src/main/java/models/User.java
+++ src/main/java/models/User.java
@@ -1,2 +1,67 @@
 package models;
 
+public class User {
+
+    private long id;
+    private String username;
+    private String email;
+    private String phone;
+    private String password;
+    double rating;
+
+    public User(){
+        // default
+    }
+
+    public long getId(){
+        return id;
+    }
+
+    public String getUsername(){
+        return username;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public String getPhone(){
+        return phone;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+
+    public double getRating(){
+        return rating;
+    }
+
+    public void setId(long id){
+        this.id = id;
+    }
+
+    public void setUsername(String username){
+        this.username = username;
+    }
+
+    public void setEmail(String email){
+        this.email = email;
+    }
+
+    public void setPhone(String phone){
+        this.phone = phone;
+    }
+
+    public void setPassword(String password){
+        this.password = password;
+    }
+
+    public void setRating(Double rating){
+        this.rating = rating;
+    }
+
+    public String toString(){
+        return "";
+    }
+}
