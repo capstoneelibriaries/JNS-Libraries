a152013c99ebd350dd95dc1d38939f81a50a27ef
jamesjmeyer210 <jamesjmeyer210@gmail.com>
2019-03-11 15:37:52 -0500

    Added transaction code

---
+++ src/main/java/models/Transaction.java
@@ -0,0 +1,57 @@
+package models;
+
+import java.util.Date;
+
+public class Transaction {
+
+    public Transaction(){
+        //default
+    }
+
+    private User buyer;
+    private User seller;
+    private Ad item;
+    private TradeRequest trade;
+    private Date date;
+
+    public User getBuyer() {
+        return buyer;
+    }
+
+    public void setBuyer(User buyer) {
+        this.buyer = buyer;
+    }
+
+    public User getSeller() {
+        return seller;
+    }
+
+    public void setSeller(User seller) {
+        this.seller = seller;
+    }
+
+    public Ad getItem() {
+        return item;
+    }
+
+    public void setItem(Ad item) {
+        this.item = item;
+    }
+
+    public TradeRequest getTrade() {
+        return trade;
+    }
+
+    public void setTrade(TradeRequest trade) {
+        this.trade = trade;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+}
