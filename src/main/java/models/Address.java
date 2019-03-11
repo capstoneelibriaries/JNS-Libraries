cd4c1815602679e80c1fb160f651a3b6e91efff5
jamesjmeyer210 <jamesjmeyer210@gmail.com>
2019-03-11 15:33:35 -0500

    Added address class

---
+++ src/main/java/models/Address.java
@@ -0,0 +1,91 @@
+package models;
+
+public class Address {
+    
+    private long id;
+    private User user;
+    private boolean billing;
+    private String streetAddr;
+    private String subAddr;
+    private String country;
+    private String city;
+    private String state;
+    private String zipcode;
+
+    public Address(){
+        // default
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public boolean isBilling() {
+        return billing;
+    }
+
+    public void setBilling(boolean billing) {
+        this.billing = billing;
+    }
+
+    public String getStreetAddr() {
+        return streetAddr;
+    }
+
+    public void setStreetAddr(String streetAddr) {
+        this.streetAddr = streetAddr;
+    }
+
+    public String getSubAddr() {
+        return subAddr;
+    }
+
+    public void setSubAddr(String subAddr) {
+        this.subAddr = subAddr;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZipcode() {
+        return zipcode;
+    }
+
+    public void setZipcode(String zipcode) {
+        this.zipcode = zipcode;
+    }
+
+}
