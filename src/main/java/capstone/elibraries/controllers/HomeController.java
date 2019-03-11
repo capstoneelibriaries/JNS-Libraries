3227eccf43c289d96419cc5fbd40e8b589d726f9
Nathaniel Gumbs <ngumbs37@gmail.com>
2019-03-11 16:57:07 -0500

    update example.properties

--- src/main/java/capstone/elibraries/controllers/HomeController.java
+++ src/main/java/capstone/elibraries/controllers/HomeController.java
@@ -1,8 +1,9 @@
 package capstone.elibraries.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
-@RestController
+@Controller
 public class HomeController {
 
     @GetMapping("/")
