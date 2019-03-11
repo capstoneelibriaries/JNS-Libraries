d929afcac32d08d2e7d9ddc2d296a7265f0acf47
Nathaniel Gumbs <ngumbs37@gmail.com>
2019-03-11 14:06:08 -0500

    update database

--- concept/init.sql
+++ concept/init.sql
@@ -4,39 +4,39 @@ USE elibs_db;
 
 
 CREATE TABLE IF NOT EXISTS users (
-  id INT AUTO_INCREMENT PRIMARY KEY,
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   is_admin BOOLEAN NOT NULL DEFAULT FALSE,
   username VARCHAR(128) NOT NULL,
   email VARCHAR(128) NOT NULL,
   password VARCHAR(128) NOT NULL,
   phone VARCHAR(20),
-  rating DOUBLE
+  rating DOUBLE UNSIGNED
 );
 -- Books
 CREATE TABLE IF NOT EXISTS books (
-  id INT AUTO_INCREMENT PRIMARY KEY,
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   isbn VARCHAR(32) NOT NULL,
   title VARCHAR(128) NOT NULL,
   author VARCHAR(128) NOT NULL,
   synopsis TEXT,
-  wear TINYINT
+  wear TINYINT(1) UNSIGNED
 );
 
 CREATE TABLE IF NOT EXISTS genres (
-  id INT AUTO_INCREMENT PRIMARY KEY,
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
   name VARCHAR(32)
 );
 
 CREATE TABLE IF NOT EXISTS genres_books (
-  genre_id INT,
-  book_id INT,
+  genre_id INT UNSIGNED,
+  book_id INT UNSIGNED,
   FOREIGN KEY (genre_id) REFERENCES genres(id),
   FOREIGN KEY (book_id) REFERENCES books(id)
 );
 
 CREATE TABLE IF NOT EXISTS ads (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  user_id INT,
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  user_id INT UNSIGNED,
   price DOUBLE,
   shipping DOUBLE,
   tradable BOOLEAN,
@@ -44,23 +44,23 @@ CREATE TABLE IF NOT EXISTS ads (
 );
 
 CREATE TABLE IF NOT EXISTS ads_books(
-  ad_id INT,
-  book_id INT,
+  ad_id INT UNSIGNED,
+  book_id INT UNSIGNED,
   FOREIGN KEY (ad_id) REFERENCES ads(id),
   FOREIGN KEY (book_id) REFERENCES books(id)
 );
 
 CREATE TABLE IF NOT EXISTS trade_requests (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  sale_ad_id INT,
-  wanted_ad_id INT,
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  sale_ad_id INT UNSIGNED,
+  wanted_ad_id INT UNSIGNED,
   FOREIGN KEY (sale_ad_id) REFERENCES ads(id),
   FOREIGN KEY (wanted_ad_id) REFERENCES ads(id)
 );
 
 CREATE Table IF NOT EXISTS addresses (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  user_id INT,
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  user_id INT UNSIGNED,
   billing BOOLEAN,
   address1 VARCHAR(256),
   address2 VARCHAR(256),
@@ -72,10 +72,10 @@ CREATE Table IF NOT EXISTS addresses (
 );
 
 CREATE TABLE IF NOT EXISTS transactions (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  ad_id INT ,
-  buyer_id INT,
-  trade_id INT,
+  id INT UNSIGNED AUTO_INCREMENT PRIMARY KEY,
+  ad_id INT UNSIGNED,
+  buyer_id INT UNSIGNED,
+  trade_id INT UNSIGNED,
   FOREIGN KEY (buyer_id) REFERENCES users(id),
   FOREIGN KEY (trade_id) REFERENCES trade_requests(id)
 );
