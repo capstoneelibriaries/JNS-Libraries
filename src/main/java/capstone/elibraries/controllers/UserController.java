d744e83db80950903a9e98bb52ad8d3bc6dd93b8
Nathaniel Gumbs <ngumbs37@gmail.com>
2019-03-13 09:31:26 -0500

    removed login listeners and added register listeners. hashes password User on creation

--- src/main/java/capstone/elibraries/controllers/UserController.java
+++ src/main/java/capstone/elibraries/controllers/UserController.java
@@ -1,20 +1,35 @@
 package capstone.elibraries.controllers;
 
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
+import capstone.elibraries.models.User;
+import capstone.elibraries.repositories.Users;
+import org.springframework.ui.Model;
 
 
 @Controller
 public class UserController {
+    private Users users;
+    private PasswordEncoder passwordEncoder;
 
-    @GetMapping("/login")
-    public String getLogin(){
-        return "/user/login";
+    public UserController(Users users, PasswordEncoder passwordEncoder) {
+        this.users = users;
+        this.passwordEncoder = passwordEncoder;
     }
 
-    @PostMapping("/login")
-    public String postLogin(){
-        return "/user/profile";
+    @GetMapping("/register")
+    public String showRegisterForm(Model model){
+        model.addAttribute("user", new User());
+        return "users/register";
+    }
+
+    @PostMapping("/register")
+    public String saveUser(@ModelAttribute User user){
+        String hash = passwordEncoder.encode(user.getPassword());
+        user.setPassword(hash);
+        users.save(user);
+    return "redirect:/login";
     }
 
 }
