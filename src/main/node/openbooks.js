23aa12838a861c7f3e0308903d54f74d5bb1e451
jamesjmeyer210 <jamesjmeyer210@gmail.com>
2019-03-13 11:58:03 -0500

    Changed request to a proper json request

--- src/main/node/openbooks.js
+++ src/main/node/openbooks.js
@@ -5,17 +5,14 @@ const http = require('http');
 
 const requestJSON = (isbn) => {
 
-    http.get(`http://openlibrary.org/api/books?bibkeys=ISBN:${isbn}`, (resp) => {
+    http.get(`http://openlibrary.org/api/books?bibkeys=ISBN:${isbn}&format=json`, (resp) => {
         let data = '';
         // A chunk of data has been recieved.
         resp.on('data', (chunk) => {
             data += chunk;
         });
-        // The whole response has been received. In order for the data to be a
-        // proper json object, we'll have to parse some of response.
+
         resp.on('end', () => {
-            data = data.replace("var _OLBookInfo = ", "");
-            data = data.replace(";", "");
             console.log(JSON.parse(data));
         });
 
