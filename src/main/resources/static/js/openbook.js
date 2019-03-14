c66fb091333c4080aaa93f3a7ec6ef569dfe8462
jamesjmeyer210 <jamesjmeyer210@gmail.com>
2019-03-14 09:56:14 -0500

    Recieve response and can properly access object

--- src/main/resources/static/js/openbook.js
+++ src/main/resources/static/js/openbook.js
@@ -1,29 +1,17 @@
 console.log("Running openbook.js");
 
+let apiResponse = {};
+
 const apiRequest = (isbn) => {
+
     $.ajax({
-        'url' : `https://openlibrary.org/api/books?bibkeys=ISBN:${isbn}&jscmd=data&format=json`,
-        'type' : 'GET',
-        // 'success' : (data) => {
-        //     return data;
-        // },
-        // 'error' : (request,error) => {
-        //     console.log("Request: "+JSON.stringify(request));
-        // },
-    }).done((data) => {
-        data = data.responseJSON;
-        return data[`ISBN:${isbn}`];
+        'url': `https://openlibrary.org/api/books?bibkeys=ISBN:${isbn}&jscmd=data&format=json`,
+        'type': 'GET',
+        'success': (data) => {
+            apiResponse = data[`ISBN:${isbn}`];
+        },
+        'error': (request, error) => {
+            console.log("Request: " + JSON.stringify(request));
+        }
     });
-};
-
-const jsonFromRequest = (request) => {
-    return request.responseJSON;
-};
-
-const objFromISBN = (isbn, json) => {
-    return json[`ISBN:${isbn}`];
-};
-
-const getBook = (isbn) => {
-    return objFromISBN(isbn, jsonFromRequest(apiRequest(isbn)));
 };
