9ff54657e20b86b094dca8623187af648b4818a7
jamesjmeyer210 <jamesjmeyer210@gmail.com>
2019-03-14 15:44:53 -0500

    created javascript object of form

--- src/main/resources/static/js/create_ad.js
+++ src/main/resources/static/js/create_ad.js
@@ -1,1 +1,25 @@
 console.log("Running create ad.js");
+
+const form = {
+    create: "create-ad",
+    price: {
+        label: "ad-price",
+        field: "create-ad-price",
+    },
+    shipping: {
+        label: "ad-shipping",
+        field: "create-ad-shipping",
+    },
+    tradable: {
+        label: "ad-tradable",
+        field: "create-ad-tradable",
+    },
+    newBook: {
+        label: "ad-new-book",
+        field: "btn-ad-new-book",
+    },
+    submit: {
+        label: "ad-submit-all",
+        field: "btn-ad-submit-all",
+    }
+};
