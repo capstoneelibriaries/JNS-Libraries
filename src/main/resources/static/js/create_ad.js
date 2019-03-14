128c5379089dc755ad332737c5b909b07d7ea205
jamesjmeyer210 <jamesjmeyer210@gmail.com>
2019-03-14 16:18:11 -0500

    jquery imbeds additional form elements to the page as book items

--- src/main/resources/static/js/create_ad.js
+++ src/main/resources/static/js/create_ad.js
@@ -1,25 +1,41 @@
 console.log("Running create ad.js");
 
 const form = {
-    create: "create-ad",
+    create: "#create-ad",
     price: {
-        label: "ad-price",
-        field: "create-ad-price",
+        label: "#ad-price",
+        field: "#create-ad-price",
     },
     shipping: {
-        label: "ad-shipping",
-        field: "create-ad-shipping",
+        label: "#ad-shipping",
+        field: "#create-ad-shipping",
     },
     tradable: {
-        label: "ad-tradable",
-        field: "create-ad-tradable",
+        label: "#ad-tradable",
+        field: "#create-ad-tradable",
     },
     newBook: {
-        label: "ad-new-book",
-        field: "btn-ad-new-book",
+        label: "#ad-new-book",
+        button: "#btn-ad-new-book",
+        form: "#ad-new-book-form",
     },
     submit: {
-        label: "ad-submit-all",
-        field: "btn-ad-submit-all",
+        label: "#ad-submit-all",
+        button: "#btn-ad-submit-all",
     }
 };
+
+const isbn_min = 10;
+const isbn_max = 13;
+let bookc = 0;
+
+$(form.newBook.button).on("click", (event) => {
+
+    let bookid = `new-book-${bookc}`;
+    $(form.newBook.form).html(
+       `<section id=${bookid}>` +
+            `<label for="${bookid}-isbn">ISBN</label>` +
+            `<input id="${bookid}-isbn" type="number" minlength="${isbn_min}" maxlength="${isbn_max}" name="${bookid}-isbn"/>` +
+        `</section>`
+    );
+});
