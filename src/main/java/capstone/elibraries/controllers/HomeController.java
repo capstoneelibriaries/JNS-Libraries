8918e3d5bed5acd23a71f63fe4b3c49bf86719e8
jamesjmeyer210 <jamesjmeyer210@gmail.com>
2019-03-14 11:02:01 -0500

    Homepage receives all ads

--- src/main/java/capstone/elibraries/controllers/HomeController.java
+++ src/main/java/capstone/elibraries/controllers/HomeController.java
@@ -1,13 +1,25 @@
 package capstone.elibraries.controllers;
 
+import capstone.elibraries.models.Ad;
+import capstone.elibraries.repositories.Ads;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @Controller
 public class HomeController {
 
+    private Ads adsDao;
+
+    public HomeController(Ads adsDao){
+        this.adsDao = adsDao;
+    }
+
     @GetMapping("/")
-    public String home(){
+    public String home(Model model){
+        model.addAttribute("ads", adsDao.findAll());
         return "index";
     }
 }
