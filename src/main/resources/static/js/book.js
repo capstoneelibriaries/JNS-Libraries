e6e0a120469b6666c8eba97f078f5f0028b6fd23
jamesjmeyer210 <jamesjmeyer210@gmail.com>
2019-03-15 14:15:36 -0500

    Book javascript file is complete

--- src/main/resources/static/js/book.js
+++ src/main/resources/static/js/book.js
@@ -1,13 +1,19 @@
 // test isbn: 0672314800
 const Book = {
+  // the template object
+  self: {
+    isbn: "",
+    title: "",
+    author: "",
+    synopsis: "",
+  },
   // constructor that creates a book from the openbook api
   from: (openbook) => {
-    let self = {
-      isbn: openbook.isbn,
-      title: openbook.title,
-      author: Book.parseAuthor(openbook),
-      synopsis: Book.parseSynopsis(openbook),
-    };
+    let self = Book.self;
+    self.isbn = openbook.isbn;
+    self.title = openbook.title;
+    self.author = Book.parseAuthor(openbook);
+    self.synopsis = Book.parseSynopsis(openbook);
     return self;
   },
   parseAuthor: (openbook) => {
@@ -19,14 +25,11 @@ const Book = {
         author += author.name + ", ";
       });
       // cut off the last comma and space
-      authors = authors.substring(0, authors.length - 2);
-      return authors;
+      return authors.substring(0, authors.length - 2);
     }
   },
   parseSynopsis: (openbook) => {
-    if(openbook.excerpts === "undefined"){
-      return "none";
-    }else if(openbook.excerpts.length == 0){
+    if(openbook.excerpts === "undefined" || openbook.excerpts.length == 0){
       return "none";
     }else{
       return openbook.excerpts[0].text;
