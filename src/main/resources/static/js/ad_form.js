ed942654b48bc38e126d39bfd5aca82340a5d618
jamesjmeyer210 <jamesjmeyer210@gmail.com>
2019-03-16 12:54:24 -0500

    Fixed tabs, added comments

--- src/main/resources/static/js/ad_form.js
+++ src/main/resources/static/js/ad_form.js
@@ -8,51 +8,56 @@ const AdForm = {
   }
 };
 
-function ImplAdForm() {
-  this.form = "#create-ad";
-  this.price = {
+function ImplAdForm(openbookApi) {
+
+    this.openbookApi = openbookApi;
+
+    this.form = "#create-ad";
+    this.price = {
       label: "#ad-price",
       field: "#create-ad-price",
-  };
-  this.shipping = {
+    };
+    this.shipping = {
       label: "#ad-shipping",
       field: "#create-ad-shipping",
-  };
-  this.tradable = {
+    };
+    this.tradable = {
       label: "#ad-tradable",
       field: "#create-ad-tradable",
-  };
-  this.book = {
+    };
+    this.book = {
       label: "#ad-new-book",
       button: "#btn-ad-new-book",
       form: "#ad-new-book-form",
-  };
+    };
 
-  this.submit = {
+    this.submit = {
       label: "#ad-submit-all",
       button: "#btn-ad-submit-all",
-  };
-  // all book forms are listed in the following array
-  this.bookforms = [];
+    };
+    // all book forms are listed in the following array
+    this.bookforms = [];
 
-  this.bookCount = () => {
-    return this.bookforms.length;
-  };
+    this.bookCount = () => {
+        return this.bookforms.length;
+    };
 
-  this.getIsbnSize = () => {
-    return AdForm.isbn;
-  };
+    this.getIsbnSize = () => {
+        return AdForm.isbn;
+    };
 
-  this.addBookForm = (bookform) => {
-    this.bookforms.push(bookform);
-    $(this.form).append(bookform.toHtml());
-
-    let isbn = "";
-    $(bookform.isbn).on("keyup", () => {
-       isbn = $(bookform.isbn).val();
-       if(isbn.length == AdForm.isbn.min || isbn.length == AdForm.isbn.max){
-           console.log(isbn);
-       }
-    });
-  };
+    this.addBookForm = (bookform) => {
+        // add the book form to a list of bookforms
+        this.bookforms.push(bookform);
+        // embed the new form in the html
+        $(this.form).append(bookform.toHtml());
+        // watch the for the user's input into the isbn field
+        let isbn = "";
+        $(bookform.isbn).on("keyup", () => {
+           isbn = $(bookform.isbn).val();
+           if(isbn.length == AdForm.isbn.min || isbn.length == AdForm.isbn.max){
+               console.log(isbn);
+           }
+        });
+    };
 }
