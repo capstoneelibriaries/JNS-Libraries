1e3661cc3171bdf97d911c449ee7ab96b28ffa30
jamesjmeyer210 <jamesjmeyer210@gmail.com>
2019-03-16 15:01:35 -0500

    Form requests get sent

--- src/main/resources/static/js/create_ad.js
+++ src/main/resources/static/js/create_ad.js
@@ -6,3 +6,30 @@ $(adform.book.button).on("click", (event) => {
   console.log($(adform.book.button));
   adform.addBookForm(BookForm.new(adform));
 });
+
+// when the submit button is pressed...
+$(adform.submit.button).click( (event) => {
+  event.preventDefault();
+
+  // if no books are provided
+  if(adform.bookCount() < 1){
+    alert("No books provided.");
+  }
+
+  let post = new FormData();
+  post.append("price", $(adform.price.field).val());
+  post.append("shipping", $(adform.shipping.field).val());
+  post.append("tradable", $(adform.tradable.field).val());
+  adform.bookforms.forEach( (bookform, index) => {
+    post.append(`book-isbn-${index}`, $(bookform.isbn).val());
+    post.append(`book-title-${index}`, $(bookform.title).val());
+    post.append(`book-author-${index}`, $(bookform.author).val());
+    post.append(`book-synopsis-${index}`, $(bookform.synopsis).val());
+    post.append(`book-wear-${index}`, $(bookform.wear).val());
+  });
+
+  let request = new XMLHttpRequest();
+  request.open("POST", "/ads/create");
+  request.send(post);
+
+});
