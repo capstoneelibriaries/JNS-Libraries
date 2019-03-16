dab72b837991ade2d3f639a79851e03a109584a7
jamesjmeyer210 <jamesjmeyer210@gmail.com>
2019-03-16 14:14:39 -0500

    Removed dead code from openbook

--- src/main/resources/static/js/openbook.js
+++ src/main/resources/static/js/openbook.js
@@ -15,21 +15,4 @@ const OpenBook = {
       });
   },
 };
-
-// let apiResponse = {};
-//
-// const apiRequest = (isbn) => {
-//
-//     $.ajax({
-//         'url': `https://openlibrary.org/api/books?bibkeys=ISBN:${isbn}&jscmd=data&format=json`,
-//         'type': 'GET',
-//         'success': (data) => {
-//             apiResponse = data[`ISBN:${isbn}`];
-//         },
-//         'error': (request, error) => {
-//             console.log("Request: " + JSON.stringify(request));
-//         }
-//     });
-// };
-
 //apiRequest("9780980200447");
