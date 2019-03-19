b89459da84a7f19daea22054d63fc66fc0b38ef3
jamesjmeyer210 <jamesjmeyer210@gmail.com>
2019-03-19 17:30:52 -0500

    Base methods for search controller

--- src/main/java/capstone/elibraries/controllers/SearchController.java
+++ src/main/java/capstone/elibraries/controllers/SearchController.java
@@ -1,17 +1,35 @@
 package capstone.elibraries.controllers;
 
-import org.hibernate.annotations.Parameter;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class SearchController {
 
-    @PostMapping("/search")
-    public String postSearch(@RequestParam String query, Model model){
-        //TODO: add search results to the model
+    private final String[] options = {"books", "ads"};
+
+    @GetMapping("/search")
+    public String getSearch(){
+        return "redirect:/";
+    }
+
+    @GetMapping("/search?q=&option={option}")
+    public String getSearch(@PathVariable String option){
+        //TODO: do stuff
         return "search";
     }
+
+    @GetMapping("/search?q={query}&option={option}")
+    public String getSearch(@PathVariable String query, @PathVariable String option){
+        // TODO: do stuff
+        return "search";
+    }
+
+    @GetMapping("/advanced?q={query}&option={option}")
+    public String getAdvanced(@PathVariable String query, @PathVariable String option){
+        // TODO: advanced search page
+        return "advanced";
+    }
+
 }
