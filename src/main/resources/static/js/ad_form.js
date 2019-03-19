97c0d269946f64c63e1e5beb72266393197b0efc
Sam Rodriguez <sam27rodriguez@gmail.com>
2019-03-19 14:49:46 -0500

    adding the title and the description to the ad.js file and the create_ad js file and the ad form js file

--- src/main/resources/static/js/ad_form.js
+++ src/main/resources/static/js/ad_form.js
@@ -15,11 +15,11 @@ function ImplAdForm(openbookApi) {
     this.form = "#create-ad";
     this.title = {
         label: "#ad-title",
-        field: "#create-ad-title"
+        field: "#create-ad-title",
     };
     this.description = {
         label: "#ad-description",
-        field: "#create-ad-description"
+        field: "#create-ad-description",
     };
     this.price = {
       label: "#ad-price",
