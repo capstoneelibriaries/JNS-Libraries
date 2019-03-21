79d6c72b6ac66f404fd97fd23671aeaa9889d112
jamesjmeyer210 <jamesjmeyer210@gmail.com>
2019-03-21 15:33:21 -0500

    Added search functionality to server

--- src/main/java/capstone/elibraries/controllers/SearchController.java
+++ src/main/java/capstone/elibraries/controllers/SearchController.java
@@ -1,21 +1,43 @@
 package capstone.elibraries.controllers;
 
+import capstone.elibraries.models.Ad;
+import capstone.elibraries.repositories.Ads;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller
 public class SearchController {
 
+    private final Ads adsDao;
     private final String[] options = {"books", "ads"};
+
+    public SearchController(Ads adsDao){
+        this.adsDao = adsDao;
+    }
 
     // The default search page
     @GetMapping("search")
-    public String getSearch(@RequestParam("q") String query, @RequestParam("option") String option){
+    public String getSearch(@RequestParam("q") String query, @RequestParam("option") String option, Model model){
         // DEBUG
         System.out.println("DEBUG: getSearch(...): query = " + query + ", option = " + option);
         // END DEBUG
-        return "index";
+
+        Iterable<Ad> results = null;
+        if(option.equals(options[0])){
+            results = adsDao.findAdByBook(query, query, query);
+        }else if(option.equals(options[1])){
+            results = adsDao.findAdByAd(query, query);
+        }else{
+            return String.format("%s", HttpStatus.BAD_REQUEST);
+        }
+
+        model.addAttribute("Ads", results);
+        return "search/index";
     }
 
     // The advanced search page
@@ -24,7 +46,7 @@ public class SearchController {
         // DEBUG
         System.out.println("DEBUG: getAdvance(...): query = " + query + ", option = " + option);
         // END DEBUG
-        return "advanced";
+        return "search/advanced";
     }
 
 }
--- src/main/java/capstone/elibraries/repositories/Ads.java
+++ src/main/java/capstone/elibraries/repositories/Ads.java
@@ -2,9 +2,18 @@ package capstone.elibraries.repositories;
 
 import capstone.elibraries.models.Ad;
 import capstone.elibraries.models.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface Ads extends CrudRepository<Ad, Long> {
+
     Iterable<Ad> findAdBySeller(User user);
+
+    @Query("FROM ads WHERE title like ?1 OR description like ?2")
+    Iterable<Ad> findAdByAd(String title, String description);
+
+    @Query("FROM ads WHERE title like ?1 OR author like ?2 OR synopsis like ?3")
+    Iterable<Ad> findAdByBook(String title, String author, String synopsis);
+
 }
 
