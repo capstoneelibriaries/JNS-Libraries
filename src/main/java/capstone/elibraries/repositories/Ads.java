a7e0cdf360f52ca4b8ae2bfc109e309f6cb485e1
jamesjmeyer210 <jamesjmeyer210@gmail.com>
2019-03-21 16:12:26 -0500

    Changed error from startup to runtime ;)

--- src/main/java/capstone/elibraries/repositories/Ads.java
+++ src/main/java/capstone/elibraries/repositories/Ads.java
@@ -4,16 +4,27 @@ import capstone.elibraries.models.Ad;
 import capstone.elibraries.models.User;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.security.access.method.P;
 
 public interface Ads extends CrudRepository<Ad, Long> {
 
     Iterable<Ad> findAdBySeller(User user);
 
-    @Query("FROM ads WHERE title like ?1 OR description like ?2")
-    Iterable<Ad> findAdByAd(String title, String description);
+    @Query(nativeQuery = true,
+            value = "SELECT * FROM ads WHERE ads.title " +
+                    "like '%:title%' " +
+                    "OR ads.description like '%:description%'")
+    Iterable<Ad> findAdByAd(@Param("title") String title, @Param("description") String description);
 
-    @Query("FROM ads WHERE title like ?1 OR author like ?2 OR synopsis like ?3")
-    Iterable<Ad> findAdByBook(String title, String author, String synopsis);
+    @Query(nativeQuery = true,
+            value = "SELECT * FROM ads " +
+                    "WHERE ads.title like '%:title%' " +
+                    "OR ads.author like '%:author%' " +
+                    "OR ads.synopsis like '%:synopsis%' ")
+    Iterable<Ad> findAdByBook(@Param("title") String title,
+                              @Param("author") String author,
+                              @Param("synopsis") String synopsis);
 
 }
 
