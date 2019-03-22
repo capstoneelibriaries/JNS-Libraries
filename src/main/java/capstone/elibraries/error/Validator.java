0b2ff8ac44b5d51ee54d81e9e2b058612039f38b
Sam Rodriguez <sam27rodriguez@gmail.com>
2019-03-22 14:19:04 -0500

    added email validation

--- src/main/java/capstone/elibraries/error/Validator.java
+++ src/main/java/capstone/elibraries/error/Validator.java
@@ -2,11 +2,36 @@ package capstone.elibraries.error;
 
 public class Validator {
 
-    public static boolean validateImageUrl(String imageUrl){
-        if(imageUrl.endsWith(".jpeg") || imageUrl.endsWith(".jpg") || imageUrl.endsWith(".png")){
+    public static boolean validateImageUrl(String imageUrl) {
+        if (imageUrl.endsWith(".jpeg") || imageUrl.endsWith(".jpg") || imageUrl.endsWith(".png")) {
             return true;
         }
         return false;
     }
+
+    public static boolean validateEmail(String email) {
+        if (email.contains("@") ||
+                email.contains(".com") ||
+                email.endsWith(".com") ||
+                email.endsWith(".net") ||
+                email.endsWith(".org") ||
+                email.endsWith(".gov") ||
+                email.endsWith(".edu")) {
+            return true;
+        } else if (email.startsWith("@") ||
+                email.startsWith(".") ||
+                email.startsWith(",") ||
+                email.startsWith(";") ||
+                email.startsWith(":") ||
+                email.contains("@.") ||
+                email.contains("`") ||
+                email.contains("~")
+        ) {
+            return false;
+        }
+        return false;
+    }
+
+
 
 }
