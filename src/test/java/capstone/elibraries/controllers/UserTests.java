db0055a45e9f1f6ab8137191d12bbdebf2abefd3
jamesjmeyer210 <jamesjmeyer210@gmail.com>
2019-03-25 17:50:01 -0500

    toString method produced a null pointer exception

--- src/test/java/capstone/elibraries/controllers/UserTests.java
+++ src/test/java/capstone/elibraries/controllers/UserTests.java
@@ -3,20 +3,36 @@ package capstone.elibraries.controllers;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
 import static org.assertj.core.api.Java6Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class UserTests {
 
     @Autowired
-    private UserController controller;
+    private MockMvc mvc;
 
+    // Sanity Test
     @Test
     public void contextLoads() throws Exception {
-        assert(controller != null);
+        assertThat(mvc).isNotNull();
+    }
+
+    @Test
+    public void defaultRegisterGetRequest() throws Exception {
+        this.mvc.perform(get("/register"))
+                .andDo(print())
+                .andExpect(status().isOk());
     }
 
 }
