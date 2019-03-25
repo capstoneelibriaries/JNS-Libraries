833b62aaf984ddda5efe7d39e63d39acee23ba8f
jamesjmeyer210 <jamesjmeyer210@gmail.com>
2019-03-25 13:45:00 -0500

    Tests for the user

--- src/test/java/capstone/elibraries/models/UserTest.java
+++ src/test/java/capstone/elibraries/models/UserTest.java
@@ -1,5 +1,7 @@
 package capstone.elibraries.models;
 
+import org.junit.Test;
+
 /*
 * Tests for the User Class in isolation
 * */
@@ -7,5 +9,66 @@ public class UserTest {
 
     private User user;
 
-    
+    private long randomLong(){
+        return (long)(Math.abs(Math.random() * 1000));
+    }
+
+    private char randomAscii(){
+        return (char)(Math.abs(Math.random() * 64) + 32);
+    }
+
+    private String randomString(int len){
+        StringBuilder rand = new StringBuilder(len);
+        for(int i = 0; i < len; i++){
+            rand.append(randomAscii());
+        }
+        return rand.toString();
+    }
+
+    /*
+    * User Id tests
+    * */
+    @Test(expected = NumberFormatException.class)
+    public void userIdCannotBeNegetive(){
+        long badId = -1 * randomLong();
+
+        user = new User();
+        user.setId(badId);
+    }
+
+    @Test
+    public void userIdGetterReturnsSetValue(){
+        long id = randomLong();
+
+        user = new User();
+        user.setId(id);
+        assert(user.getId() == id);
+    }
+
+    @Test
+    public void userIdGetterReturnsZeroWhenNotInstantiated(){
+        user = new User();
+        assert(user.getId() == 0);
+    }
+
+    /*
+    * User name tests
+    * */
+    @Test(expected = NullPointerException.class)
+    public void userNameCannotBeNull(){
+        user = new User();
+        user.setUsername(null);
+    }
+
+    @Test(expected = Exception.class)
+    public void userNameCannotBeEmpty(){
+        user = new User();
+        user.setUsername("");
+    }
+
+    @Test(expected = Exception.class)
+    public void userNameCannotBeOversized(){
+        user = new User();
+        user.setUsername(randomString(200));
+    }
 }
