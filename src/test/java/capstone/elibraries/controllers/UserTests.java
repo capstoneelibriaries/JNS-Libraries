276959990f74bfac4b017abcb3a367ba763895fb
jamesjmeyer210 <jamesjmeyer210@gmail.com>
2019-03-25 18:21:32 -0500

    Wrote a functional post test

--- src/test/java/capstone/elibraries/controllers/UserTests.java
+++ src/test/java/capstone/elibraries/controllers/UserTests.java
@@ -5,14 +5,17 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.assertj.core.api.Java6Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -29,10 +32,23 @@ public class UserTests {
     }
 
     @Test
-    public void defaultRegisterGetRequest() throws Exception {
+    public void testGetRegister() throws Exception {
         this.mvc.perform(get("/register"))
                 .andDo(print())
                 .andExpect(status().isOk());
+    }
+
+    @Test
+    public void testPostRegister() throws Exception {
+        this.mvc.perform(post("/register")
+                .param("username", "alice")
+                .param("email", "alice@email.com")
+                .param("phone", "2223334455")
+                .param("password", "password123")
+                .param("confirmPassword", "password123")
+                .contentType(MediaType.APPLICATION_FORM_URLENCODED))
+                .andExpect(status().isFound())
+                .andExpect(redirectedUrl("/login"));
     }
 
 }
