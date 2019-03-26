d2fad90e143c210731762a867c3068b15baa9974
jamesjmeyer210 <jamesjmeyer210@gmail.com>
2019-03-26 10:50:10 -0500

    Began testing Transcation Class

---
+++ src/test/java/capstone/elibraries/models/TransactionTest.java
@@ -0,0 +1,56 @@
+package capstone.elibraries.models;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class TransactionTest {
+
+    @Test
+    public void getBuyer() {
+    }
+
+    @Test
+    public void setBuyer() {
+    }
+
+    @Test
+    public void getSeller() {
+    }
+
+    @Test
+    public void setSeller() {
+    }
+
+    @Test
+    public void getItem() {
+    }
+
+    @Test
+    public void setItem() {
+    }
+
+    @Test
+    public void getDate() {
+    }
+
+    @Test
+    public void setDate() {
+    }
+
+    @Test
+    public void getId() {
+    }
+
+    @Test
+    public void setId() {
+    }
+
+    @Test
+    public void getTrade() {
+    }
+
+    @Test
+    public void setTrade() {
+    }
+}
