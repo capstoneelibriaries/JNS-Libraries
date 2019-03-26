ad19270bb0639cbb30f324bb7679bd27f484756a
jamesjmeyer210 <jamesjmeyer210@gmail.com>
2019-03-26 11:04:14 -0500

    A basic set of tests for the transaction class

--- src/test/java/capstone/elibraries/models/TransactionTest.java
+++ src/test/java/capstone/elibraries/models/TransactionTest.java
@@ -1,56 +1,103 @@
 package capstone.elibraries.models;
 
+import capstone.elibraries.error.ValidationException;
 import org.junit.Test;
 
+import static capstone.elibraries.testhelpers.DataGenerator.randomLong;
 import static org.junit.Assert.*;
 
 public class TransactionTest {
 
-    @Test
-    public void getBuyer() {
+    private Transaction trn;
+
+//    @Test
+//    public void getBuyer() {
+//    }
+//
+//    @Test
+//    public void setBuyer() {
+//    }
+
+    @Test(expected = ValidationException.class)
+    public void testBuyerCannotBeNull() throws Exception{
+        trn = new Transaction();
+        trn.setBuyer(null);
     }
 
-    @Test
-    public void setBuyer() {
+//    @Test
+//    public void getSeller() {
+//    }
+//
+//    @Test
+//    public void setSeller() {
+//    }
+
+    @Test(expected = ValidationException.class)
+    public void testSellerCannotBeNull() throws Exception {
+        trn = new Transaction();
+        trn.setSeller(null);
     }
 
-    @Test
-    public void getSeller() {
+//    @Test
+//    public void getItem() {
+//    }
+//
+//    @Test
+//    public void setItem() {
+//    }
+
+    @Test(expected = ValidationException.class)
+    public void testItemCannotBeNull() throws Exception {
+        trn = new Transaction();
+        trn.setItem(null);
     }
 
-    @Test
-    public void setSeller() {
-    }
+//    @Test
+//    public void getDate() {
+//    }
+//
+//    @Test
+//    public void setDate() {
+//    }
 
     @Test
-    public void getItem() {
+    public void testDateIsSetByDefault() throws Exception {
+        trn = new Transaction();
+
+        assert(!trn.getDate().equals(null));
     }
 
-    @Test
-    public void setItem() {
+    @Test(expected = ValidationException.class)
+    public void testDateCannotBeNull() throws Exception {
+        trn = new Transaction();
+        trn.setDate(null);
     }
 
-    @Test
-    public void getDate() {
+//    @Test
+//    public void getId() {
+//    }
+//
+//    @Test
+//    public void setId() {
+//    }
+
+    @Test(expected = ValidationException.class)
+    public void testIdCannotBeNegetive() throws Exception {
+        trn = new Transaction();
+        trn.setId(-1 * randomLong());
     }
 
-    @Test
-    public void setDate() {
-    }
+//    @Test
+//    public void getTrade() {
+//    }
+//
+//    @Test
+//    public void setTrade() {
+//    }
 
-    @Test
-    public void getId() {
-    }
-
-    @Test
-    public void setId() {
-    }
-
-    @Test
-    public void getTrade() {
-    }
-
-    @Test
-    public void setTrade() {
+    @Test(expected = ValidationException.class)
+    public void testTradeCannotBeNull() throws Exception {
+        trn = new Transaction();
+        trn.setTrade(null);
     }
 }
