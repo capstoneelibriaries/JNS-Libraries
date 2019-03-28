7e8d2d80153bc5d190882d6f2e5e0d759dff1dee
Sam Rodriguez <sam27rodriguez@gmail.com>
2019-03-28 16:00:27 -0500

    finished seeder for now

--- src/main/sql/seeder.sql
+++ src/main/sql/seeder.sql
@@ -12,38 +12,38 @@ truncate trade_requests;
 truncate transactions;
 SET FOREIGN_KEY_CHECKS = 1;
 INSERT into users (email, is_admin, password, phone, rating, username)
-VALUES ('fancyreader117@gmail.com',false, '$2a$10$jBnR8Hx2QCT77PzEnTLlxOOeNyWxhFNPIW89WKVAQYmkPkC2TVbtu', '',0,'fancyreader117'),
-       ('mike@gmail.com',false,'$2a$10$jBnR8Hx2QCT77PzEnTLlxOOeNyWxhFNPIW89WKVAQYmkPkC2TVbtu','',0,'mike'),
-       ('admin@adm.in',true,'$2a$10$jBnR8Hx2QCT77PzEnTLlxOOeNyWxhFNPIW89WKVAQYmkPkC2TVbtu','',0,'admin'),
-       ('comicmeister@gmail.com',false,'$2a$10$jBnR8Hx2QCT77PzEnTLlxOOeNyWxhFNPIW89WKVAQYmkPkC2TVbtu','',0,'comicmeister'),
-       ('harrypotterfan@gmail.com',false,'$2a$10$jBnR8Hx2QCT77PzEnTLlxOOeNyWxhFNPIW89WKVAQYmkPkC2TVbtu','',0,'hp_fan');
+VALUES ('sam@gmail.com',false, '$2a$10$jBnR8Hx2QCT77PzEnTLlxOOeNyWxhFNPIW89WKVAQYmkPkC2TVbtu', '1234567899',5,'sam'),
+       ('james@gmail.com',false,'$2a$10$jBnR8Hx2QCT77PzEnTLlxOOeNyWxhFNPIW89WKVAQYmkPkC2TVbtu','2345678999',5,'james'),
+       ('nathaniel@gmail.com',false,'$2a$10$jBnR8Hx2QCT77PzEnTLlxOOeNyWxhFNPIW89WKVAQYmkPkC2TVbtu','2345678999',5,'nathaniel'),
+       ('admin@gmail.com',true,'$2a$10$jBnR8Hx2QCT77PzEnTLlxOOeNyWxhFNPIW89WKVAQYmkPkC2TVbtu','3456789999',5,'admin');
 
 INSERT into books (author, isbn, synopsis, title, wear, image_url)
-VALUES ("Zhizhong Cai", "0385472579", "", "Zen speaks", 3, "/images/bookexample.jpeg"),
-       ("Wallace B. McClure ... [et al.].", "9780470112830", "", "Beginning asp.net 2.0 AJAX", 4.4, "/images/bookexample.jpeg"),
-       ("Patrick Koetter, Ralf Hildebrandt", "9781593270018", "", "The Book of Postfix", 2, "/images/bookexample.jpeg"),
-       ("Dave Taylor, James C. Armstrong, Jr.", "0672314800", "", "Sams teach yourself UNIX in 24 hours", 1, ""),
-       ("John R. Levine, Margaret Levine Young, Carol Baroudi", "0764589962", "", "The Internet For Dummies (Internet for Dummies)", 3, ""),
-       ("Jeanie Daniel Duck", "0609808818", "", "The change monster", 2, "");
+
+VALUES ("Harper Lee", "0060935464", "o Kill a Mockingbird takes place in the fictional town of Maycomb, Alabama, during the Great Depression. The protagonist is Jean Louise (“Scout”) Finch, an intelligent though unconventional girl who ages from six to nine years old during the course of the novel. She is raised with her brother, Jeremy Atticus (“Jem”), by their widowed father, Atticus Finch. He is a prominent lawyer who encourages his children to be empathetic and just. He notably tells them that it is “a sin to kill a mockingbird,” alluding to the fact that the birds are innocent and harmless.", "To Kill A Mocking Bird", 3, "https://covers.openlibrary.org/b/id/8410894-L.jpg"),
+
+       ("Madeleine L''Engle", "0374386137",
+        "Meg Murry and her little brother, Charles Wallace, have been without their scientist father, Mr. Murry, for five years, ever since he discovered a new planet and used the concept known as a tesseract to travel there. Joined by Meg''s classmate Calvin O''Keefe and guided by the three mysterious astral travelers known as Mrs. Whatsit, Mrs. Who and Mrs. Which, the children brave a dangerous journey to a planet that possesses all of the evil in the universe.", "A Wrinkle in Time", 2, "https://covers.openlibrary.org/b/id/7883940-L.jpg"),
+
+       ("George Orwell", "0679417397",
+        "A man loses his identity while living under a repressive regime. In a story based on George Orwell''s classic novel, Winston Smith (John
+        Hurt) is a government employee whose job involves the rewriting of history in a manner that casts his fictional country''s leaders in a charitable light. His trysts with Julia (Suzanna Hamilton) provide his only measure of enjoyment, but lawmakers frown on the relationship -- and in this closely monitored society, there is no escape from Big Brother.", "Nineteen eighty-four", 4, "https://covers.openlibrary.org/b/id/417615-L.jpg"),
+
+       ("John Steinbeck", "0140177396", "Of Mice and Men is a novella written by author John Steinbeck. Published in 1937, it tells the story of George Milton and Lennie Small, two displaced migrant ranch workers, who move from place to place in California in search of new job opportunities during the Great Depression in the United States.", "Of Mice and Men", 1, "https://covers.openlibrary.org/b/id/96845-L.jpg"),
+
+       ("Yann Martel", "9780156027328",
+        "First published in 2002, Martel''s breathtaking breakout novel became an international bestseller and went on to win the Man Booker Prize,
+         and was also named Amazon.com''s Best Book of 2002. In 2005, after an international competition, Croatian artist Tomislav Torjanac was selected to illustrate a special edition of Life of Pi that features 40 stunning illustrations that present a new perspective on this modern classic. --Brad Thomas Parsons ", "Life of Pi : a novel", 3, "https://covers.openlibrary.org/b/id/8082619-L.jpg");
+
 
 insert into ads (ad_title, description, price, shipping, tradable, user_id, pending)
-VALUES ("Ipsum dolor", "Morbi dictum dui diam, ut luctus quam commodo sed. Morbi in porta lectus. Integer dapibus justo est.", 14.99, 5.67, true, 1, true),
-       ("Lorem dolor", "Suspendisse fringilla sollicitudin arcu. Aenean egestas justo lacus, sit amet pretium neque ullamcorper eget. In in metus commodo, dapibus turpis in, ultrices tortor. Proin sodales at libero malesuada imperdiet.", 12.25, 0.0, true, 1, true),
-       ("Mercator in foro", "Nullam et arcu lorem. Aenean sed orci dui. Suspendisse id enim eget massa ullamcorper aliquet. Aliquam vehicula tellus eu nisl semper porta. Sed a dolor nec lacus dapibus egestas. Proin lobortis congue maximus.", 28.00, 0.0, true, 2, true),
-       ("Laborat", "Maecenas consectetur sed nunc pharetra varius. Ut imperdiet libero metus, sit amet rutrum mi egestas at. Ut et tortor vel nisl imperdiet dapibus varius ut ex. Suspendisse eu ligula enim. Phasellus rhoncus porta risus, sit amet lacinia leo maximus eu. Vivamus interdum.", 25.30, 9.40, true, 4, true),
-       ("Morbi at lacus lorem", "Ut eget elit sed sapien malesuada tincidunt. Etiam maximus ligula vitae mauris varius, ut fringilla sem congue. Mauris et hendrerit est. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Vestibulum at volutpat ligula. Orci varius natoque penatibus.", 7.99, 4.33, true, 2, true),
-       ("Nam id ex neque", "Quisque in lobortis dui. In sollicitudin augue et neque vehicula venenatis. Quisque eros lorem, fermentum aliquet arcu sit amet, dapibus dignissim nulla. Cras ultrices odio massa, a ullamcorper ligula fringilla non. Integer sed congue mauris. Aenean vitae lectus metus. Pellentesque euismod interdum.", 4.83, 12.6, true, 4, true),
-       ("Curabitur imperdiet", "Curabitur maximus consequat efficitur. Praesent consequat faucibus diam sed blandit. In placerat urna dolor, vel aliquam odio blandit eget. Interdum et malesuada fames ac ante ipsum primis in faucibus. Donec vel varius justo. Nullam vel leo arcu. Duis at tortor et ante.", 44.44, 5.00, true, 5, true);
+VALUES
+("Looking to trade", "I am interested in trading my copy of To Kill A mockingbird",8.99 ,2.78, true,1,true),
+("Trade Please!", "I have had this book for too long, someone take it off my hands.",4.99 ,6.78, true,1,true),
+("Interested in trading", "This is a George Orwell classic, one of my favorites.",9.99 ,3.78, true,1,true),
+("Anyone want this?", "Found this really old book in my attic, I'm looking for life of pi if anyone has it?",10.99 ,3.78, true,1,true);
 
 insert into ads_books (ad_id, book_id)
 VALUES (1, 1),
-       (1, 2),
-       (2, 4),
-       (3, 1),
-       (3, 6),
-       (4, 5),
-       (5, 2),
-       (5, 6),
-       (6, 2),
-       (7, 5),
-       (7, 6);
+       (2, 2),
+       (3, 3),
+       (4,4);
